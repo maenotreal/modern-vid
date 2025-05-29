ef3d62904b62c3d4879ec124e4e6135723b64754
@Alni1 <konevichenko111@gmail.com>
2025-05-29 14:52:34 +0300

    Refactor and plater.ejs, video.ejs, vidCache added, modified app.js

--- app.js
+++ app.js
@@ -3,11 +3,43 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+const videoRouter = require('./routes/video');
+
 var app = express();
+
+// Firebase and MongoDB setup
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
+};
+
+// Подключение к MongoDB для кеширования
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// Автоочистка кеша
+const VideoCache = require('./models/VideoCache');
+
+setInterval(async () => {
+  await VideoCache.deleteMany({ 
+    $or: [
+      { expiresAt: { $lt: new Date() } },
+      { lastAccessed: { $lt: new Date(Date.now() - 3600000) } }
+    ]
+  });
+}, 3600000);
+
+const firebaseApp = initializeApp(firebaseConfig);
+const storage = getStorage(firebaseApp);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
