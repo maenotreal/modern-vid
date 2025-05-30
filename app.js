36b0acc3e7e0285a3ac77ca7bcaf7af17d5027bb
@Alni1 <konevichenko111@gmail.com>
2025-05-30 16:40:48 +0300

    firebase fixes

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var mongoose = require('mongoose');
 
+const {initializeApp} = require('firebase-admin/app');
+const {getStorage} = require('firebase-admin/storage');
+
+
 //dotenv access
 require('dotenv')
     .config('/.env');
@@ -30,6 +34,18 @@ const searchRouter = require('./routes/search');
 var usersRouter = require('./routes/users');
 var registrationRouter = require('./routes/registration');
 var loginRouter = require('./routes/login');
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const storage = getStorage(firebaseApp);
 
 
 //mongoDB connection setup
@@ -70,5 +86,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
-module.exports = app;
+module.exports = {
+  app,
+  firebaseStorage
+}
