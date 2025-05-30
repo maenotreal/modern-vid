9aeeabab02bede5497c961b028308fb25e580032
@Alni1 <konevichenko111@gmail.com>
2025-05-30 13:18:13 +0300

    copy

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,6 @@ const User = require('../schemas/userSchema');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-<<<<<<< HEAD
   res.render('index', { title: 'Express' });
   /*if (req.cookies.token !== undefined) {
     let verificationState = jsonWT.verify(req.cookies.token, process.env.SECRET_KEY);
@@ -16,11 +15,3 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
-=======
-    const videoId = 'your-video.mp4';
-    res.render('player', {
-    videoUrl: `/video/${videoId}`
-    });
-});
-module.exports = router;
->>>>>>> accac8843ad9b0a81ba5545c79e32ffae7aaa4f0
